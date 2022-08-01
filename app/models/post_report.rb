7edbbc758a4a7eccf8a7fca20ca22504e96e7dd4
Daniel Pat <danielpat6@gmail.com>
2022-08-01 13:22:11 -0500

    report

--- app/models/post_report.rb
+++ app/models/post_report.rb
@@ -11,12 +11,12 @@ class PostReport < Struct.new(:word_count, :word_histogram)
 
 	private
 	def self.calc_historgram(post)
-		post
+		(post
 			.content
 			.split
 			.map { |word| word.gsub(/\W/,'') }
 			.map(&:downcase)
 			.group_by { |word| word }
-			.transform_values(&:size)
+			.transform_values(&:size))
 	end
 end
