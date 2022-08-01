569dfd7f53477b55afddb7f9091d5999b7308e6a
Daniel Pat <danielpat6@gmail.com>
2022-08-01 09:46:54 -0500

    generate auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
     has_many :posts
     validates :email, :name, :auth_token, presence: true
+
+    after_initialize :generate_auth_token
+
+    def generate_auth_token
+        unless auth_token.present?
+            self.auth_token = TokenGenerationService.generate
+        end
+    end
 end
