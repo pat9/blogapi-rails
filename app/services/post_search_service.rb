c4820d87b50ae2f8f3feeba8ee0bf2a9338b9f6c
Daniel Pat <danielpat6@gmail.com>
2022-08-01 12:20:42 -0500

    add caching

--- app/services/post_search_service.rb
+++ app/services/post_search_service.rb
@@ -1,5 +1,8 @@
 class PostSearchService
     def self.search(curr_post, query)
-        curr_post.where("title like '%#{query}%'")
+        posts_ids = Rails.cache.fetch("posts_search", expires_in: 1.hours) do
+            curr_post.where("title like '%#{query}%'").map(&:id)
+        end
+        curr_post.where(id: posts_ids)
     end
 end
